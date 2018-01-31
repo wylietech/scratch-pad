49a1a98906dce0d68280aec2d61dab03d5dede71
Matthew Wylie <matthewwylie@Matthews-MacBook-Pro-2.local>
2018-01-31 21:07:10 +0000

    Hello World

---
+++ src/main/java/com/wylietech/demo/Launcher.java
@@ -0,0 +1,13 @@
+package com.wylietech.demo;
+
+public class Launcher {
+
+    /**
+     * Entry point
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
